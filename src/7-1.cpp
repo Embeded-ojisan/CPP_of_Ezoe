d6e35b71df2c4966a6e318706413888dc41a7a36
Local PC <lowlevelengineer@gmail.com>
2022-10-24 22:29:20 +0900

    origin

---
+++ src/7-1.cpp
@@ -0,0 +1,31 @@
+int main()
+{
+    double height = 1.63;
+    double mass = 73.0;
+
+    double bmi = mass / (height*height);
+
+    std::cout << "BMI="s << bmi << "\n"s;
+
+    auto status = [](double bmi)
+    {
+        if(bmi < 18.5)
+        {
+            return "Underweight.\n"s;
+        }
+        else if(bmi < 25.0)
+        {
+            return "Normal. \n"s;
+        }
+        else if(bmi < 30.0)
+        {
+            return "Overweight"s;
+        }
+        else
+        {
+            return "Obese. "s;
+        }
+    };
+
+    std::cout MM status(bmi);
+}
